50ef4f1aa45fb1d95e0da1d96f9fc3884b03acfd
AMT-Technology <des.amt.01@gmail.com>
2025-12-29 19:04:09 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,17 @@
 // Configuración Firebase
-const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyCEMOAiEhDC-Z5nS4_yB-a1s3T_61PrHF4",
   authDomain: "appwebs-8b23f.firebaseapp.com",
   projectId: "appwebs-8b23f",
-  storageBucket: "appwebs-8b23f.firebaseapp.com",
+  storageBucket: "appwebs-8b23f.appspot.com",
   messagingSenderId: "986164552498",
-  appId: "1:986164552498:web:04262bde6fa35e5efced2b",
-  measurementId: "G-63HNLTNVY0"
+  appId: "1:986164552498:web:04262bde6fa35e5efced2b"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Inicializar Firebase (v8)
+firebase.initializeApp(firebaseConfig);
 
-// Servicios
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
+// 👉 HACER GLOBALES
+window.auth = firebase.auth();
+window.db = firebase.firestore();
+window.storage = firebase.storage();
